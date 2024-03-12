eba2bcf5805f2a5ed1e109a18cb9429b308afef3
ademelmae <ademelviagedor22@gmail.com>
2024-03-12 16:07:17 +0800

    created design for rider dashboard(unfinished)

---
+++ src/components/RiderProfile.jsx
@@ -0,0 +1,56 @@
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import {
+    faCircleUser,
+    faSearch
+} from "@fortawesome/free-solid-svg-icons";
+import { Col, Row, Input } from 'reactstrap';
+const RiderProfile = () => {
+    return (
+        <>
+            <div className="box-header">
+                <div className="rectangle-header">
+                    <div className="label">
+                        <div className="text-wrapper">Rider Profile</div>
+                    </div>
+                </div>
+
+                <div className="rect-profile">
+                    <Row>
+                        <Col md={3}>
+                            <Icon className="profile-img" icon={faCircleUser} color='black' style={{ fontSize: "150px" }} />
+                        </Col>
+                        <Col md={9}>
+                            <Row className="name-row">
+                                <div className="text-name">Juan Dela Cruz</div>
+                            </Row>
+                            <Row className="details-row">
+                                <div className="text-details">Address: Sudlonon Bogo City, Cebu
+                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                    Position : Cab Rider
+                                    <br></br>
+                                    Age:20
+                                    &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                    Vehicle No: 47548
+                                    <br></br>
+                                    Contact Number: 09564457889
+                                    <br></br>
+
+                                </div>
+                            </Row>
+                        </Col>
+                    </Row>
+                </div>
+
+
+                <div className="search-box">
+                    <input type="text" placeholder="Search for rider" />
+                </div>
+
+            </div>
+        </>
+    );
+}
+
+export default RiderProfile;
