76fbbfd2aebdfa77a7ec887a65f943923da25378
stewart065 <clientstewart21@gmail.com>
2024-03-12 15:58:38 +0800

    partial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
 import HomePage from "./routes/HomePage";
-import CardTotalRider from './CardTotalRider'
-
 function App() {
   return (
     <>
       <HomePage />
-      <CardTotalRider />
     </>
   );
 }
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,9 +1,15 @@
 import { Row, Col } from "reactstrap";
 import NavigationBar from "../components/NavigationBar";
+import CardTotalRider from "../components/CardTotalRider";
+import AVGCommuters from "../components/AVG-Commuters";
+import AVGrevenue from "../components/AVG-Revenue";
 const HomePage = () => {
   return (
     <>
       <NavigationBar />
+      <CardTotalRider/>
+      <AVGCommuters/>
+      <AVGrevenue/>
     </>
   );
 };
