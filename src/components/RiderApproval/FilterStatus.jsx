66e0898c37932665728ef2521300c4b7358fe943
sinadjanMJ <sinadjanmichaeljay@gmail.com>
2024-03-20 10:22:35 +0800

    fixing some functionalities

--- src/components/RiderApproval/FilterStatus.jsx
+++ src/components/RiderApproval/FilterStatus.jsx
@@ -1,16 +1,45 @@
 import React from "react";
-import { Button } from "reactstrap";
+import { useState } from "react";
+import {
+  Button,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownItem,
+  DropdownMenu,
+  Label,
+  Input,
+  FormGroup,
+} from "reactstrap";
 
 
 const FilterStatus = () => {
+
+  const StatusData = [
+    { id: 55, status: "Pending" },
+    { id: 56, status: "Approved" },
+    { id: 57, status: "Rejected" },
+  ];
 
   return (
     <>
 
     
-   <Button className="filter-status">
-    Filter by status
-  </Button>
+ 
+      <UncontrolledDropdown>
+        <DropdownToggle
+          caret
+          color="dark"
+          className="rider-approval-filter-status"
+        >
+          Filter by status
+        </DropdownToggle>
+        <DropdownMenu dark>
+          <DropdownItem header>Select to Filter</DropdownItem>
+          {StatusData.map((item) => (
+            <DropdownItem key={item.id}>{item.status}</DropdownItem>
+          ))}
+        </DropdownMenu>
+      </UncontrolledDropdown>
 
   </>
   )
