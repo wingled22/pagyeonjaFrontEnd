7f13c179440a6e5f6ca3ef89d9ab5b9207675f22
onin sama <ninoabao@gmail.com>
2024-03-21 14:51:15 +0800

    Map the approval list for riders.

--- src/components/RiderApproval/Rider_Approval_Table.jsx
+++ src/components/RiderApproval/Rider_Approval_Table.jsx
@@ -6,6 +6,7 @@ import images1 from "../../assets/image/carlo.jpg";
 import images2 from "../../assets/image/cliff.jpg";
 import images3 from "../../assets/image/cs3.png";
 import RiderApprovalSearchFilter from "./RiderApprovalSearchAndFilter";
+import { Row } from "reactstrap";
 
 const Rider_Approval_TablePage = ({ text, color, changeUserID }) => {
   const [approvals, setApprovals] = useState([]);
@@ -24,57 +25,61 @@ const Rider_Approval_TablePage = ({ text, color, changeUserID }) => {
 
   const callChangeUserID = (id) => {
     changeUserID(id);
-
-    const [searchTerm, setSearchTerm] = useState("");
-
-    const handleSearch = (value) => {
-      setSearchTerm(value);
-    };
-
-    const filteredData = approvals.filter(
-      (item) =>
-        item.name.toLowerCase().includes(searchTerm) ||
-        item.status.toLowerCase().includes(searchTerm)
-    );
   };
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearch = (value) => {
+    setSearchTerm(value);
+  };
+
+  const filteredData = approvals.filter(
+    (item) =>
+      item.firstName.toLowerCase().includes(searchTerm) ||
+      item.lastName.toLowerCase().includes(searchTerm)
+    // item.approvalStatus.toLowerCase().includes(searchTerm)
+  );
   useEffect(() => {
     getApprovalList();
   }, []); // Add isFetched as a dependency
 
   return (
-    <div className="rider-approval-table-container">
-      <table className="table-in">
-        <tbody>
-          {filteredData.map((item) => (
-            <tr
-              key={item.id}
-              onClick={() => {
-                callChangeUserID(item.id);
-              }}
-            >
-              <td className="td-style">
-                <img
-                  src={
-                    item.profilePath != null
-                      ? `http://localhost:5180/img/rider_profile/${item.profilePath}`
-                      : images1
-                  } // use default image if profilePath is null
-                  className="rider-table-image"
-                />
-              </td>
-              <td className="td-style">
-                {item.firstName} {item.middleName} {item.lastName}
-              </td>
-              <td className="td-style">
-                <Badge
-                  text={item.approvalStatus === true ? "Approved" : "Pending"}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <Row>
+        <RiderApprovalSearchFilter onSearch={handleSearch} />
+      </Row>
+      <div className="rider-approval-table-container">
+        <table className="table-in">
+          <tbody>
+            {filteredData.map((item) => (
+              <tr
+                key={item.id}
+                onClick={() => {
+                  callChangeUserID(item.id);
+                }}
+              >
+                <td className="td-style">
+                  <img
+                    src={
+                      item.profilePath != null
+                        ? `http://localhost:5180/img/rider_profile/${item.profilePath}`
+                        : images1
+                    } // use default image if profilePath is null
+                    className="rider-table-image"
+                  />
+                </td>
+                <td className="td-style">
+                  {item.firstName} {item.middleName} {item.lastName}
+                </td>
+                <td className="td-style">
+                  <Badge
+                    text={item.approvalStatus === true ? "Approved" : "Pending"}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
