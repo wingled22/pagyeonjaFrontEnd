93cbd61fc0c365adf7993867728fb1617370696f
sinadjanMJ <sinadjanmichaeljay@gmail.com>
2024-03-22 16:04:25 +0800

    fix filter status

--- src/components/RiderApproval/RiderApprovalSearchAndFilter.jsx
+++ src/components/RiderApproval/RiderApprovalSearchAndFilter.jsx
@@ -1,16 +1,8 @@
-import React from "react";
+import React, {useState}from "react";
 import "../../assets/css/RiderApproval/RiderApprovalDashboard.css";
-import {
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownItem,
-  DropdownMenu,
-  Col,
-  Row,
-} from "reactstrap";
+import {UncontrolledDropdown,DropdownToggle,DropdownItem,DropdownMenu,Col,Row,FormGroup,Input,} from "reactstrap";
 
-const RiderApprovalSearchFilter = ({onSearch }) => {
-
+const RiderApprovalSearchFilter = ({ onSearch }) => {
   const StatusData = [
     { id: 55, status: "Pending" },
     { id: 56, status: "Approved" },
@@ -20,40 +12,48 @@ const RiderApprovalSearchFilter = ({onSearch }) => {
     onSearch(event.target.value.toLowerCase());
   };
 
+  const [selectedStatus, setSelectedStatus] = useState(null);
+
+  const handleStatusSelect = (status) => {
+    setSelectedStatus(status);
+  };
+
   return (
     <>
-    <Row sm={11}>
-    
-    <Col xs={9} sm={9} md={8} lg={9} xl={9} className="me-xl-3 me-lg-3 me-md-3">
-        <input
-          type="text"
-          className="form-control search-field"
-          placeholder="Search name" 
-          onChange={handleSearch}
-        />
-      </Col>
-
-
-   <Col xs={1} sm={2} md={2} lg={1} xl={1} >
-      <UncontrolledDropdown>
-        <DropdownToggle
-          caret
-          color="dark"
-          className="rider-approval-filter-status"
+      <Row sm={11}>
+        <Col
+          xs={9}
+          sm={9}
+          md={8}
+          lg={9}
+          xl={9}
+          className="me-xl-3 me-lg-3 me-md-3"
         >
-          Filter by status
-        </DropdownToggle>
-        <DropdownMenu dark>
-          <DropdownItem header>Select to Filter</DropdownItem>
-          {StatusData.map((item) => (
-            <DropdownItem key={item.id}>{item.status}</DropdownItem>
-          ))}
-        </DropdownMenu>
-      </UncontrolledDropdown>
+          <input
+            type="text"
+            className="form-control search-field"
+            placeholder="Search name"
+            onChange={handleSearch}
+          />
+        </Col>
 
-    </Col>
-    </Row>
-    
+        <Col xs={1} sm={2} md={2} lg={1} xl={1}>
+          
+        <UncontrolledDropdown>
+      <DropdownToggle caret color="dark" className="rider-approval-filter-status">
+        {selectedStatus ? selectedStatus : 'Filter by status'}
+      </DropdownToggle>
+      <DropdownMenu dark>
+        <DropdownItem header>Select to Filter</DropdownItem>
+        {StatusData.map((item) => (
+          <DropdownItem key={item.id} onClick={() => handleStatusSelect(item.status)}>
+            {item.status}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </UncontrolledDropdown>
+        </Col>
+      </Row>
     </>
   );
 };
