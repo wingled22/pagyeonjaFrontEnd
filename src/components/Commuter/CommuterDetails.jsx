39aacb7ca410fa0ed13a86b03ab66f4b5015239b
ademelmae <ademelviagedor22@gmail.com>
2024-03-22 11:54:15 +0800

    fixed nullable profile path partial again

--- src/components/Commuter/CommuterDetails.jsx
+++ src/components/Commuter/CommuterDetails.jsx
@@ -48,7 +48,7 @@ const CommuterDetails = ({selectedCommuter}) => {
             <Row>
                 <Col md="2" sm="2" xs={12}>
               
-                {commuterInfo.profilePath === null ? <Icon icon={faCircleUser} color='black' className="iconContainer"></Icon> : <img className="imageContainer" src={`http://localhost:5180/img/commuter_profile/${commuterInfo.profilePath}`} alt="" /> }
+                {commuterInfo.profilePath === "" ? <Icon icon={faCircleUser} color='black' className="iconContainer"></Icon> : <img className="imageContainer" src={`http://localhost:5180/img/commuter_profile/${commuterInfo.profilePath}`} alt="" /> }
                
                 </Col>
                 <Col md="6" sm="6" xs={12} id="textInfoContainer">
