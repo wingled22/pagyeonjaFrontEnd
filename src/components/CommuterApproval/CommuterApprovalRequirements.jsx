d297602b2a2e93e39e7672dab07e01bc45c5a1e8
oninsan <kokoybaldofordawin@gmail.com>
2024-04-04 10:42:06 +0800

    Toggled the approval modal for Commuter approval

--- src/components/CommuterApproval/CommuterApprovalRequirements.jsx
+++ src/components/CommuterApproval/CommuterApprovalRequirements.jsx
@@ -17,6 +17,9 @@ import { auto } from "@popperjs/core";
 
 // import ViewRequirements from "./RequirementsCards";
 import CommuterDocumentViewerModal from "../Commuter/CommuterDocumentViewerModal";
+// import RiderApprovalResponseConfirmationModal from "./ApprovalResponseConfirmationModal";
+import RiderApprovalResponseConfirmationModal from "../RiderApproval/ApprovalResponseConfirmationModal";
+
 import CommuterApprovalViewRequirements from "./CommuterApprovalRequirementsCards";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
@@ -28,9 +31,21 @@ const CommuterApprovalRequirements = ({ userId, getApprovals }) => {
   console.log("requirements", userId);
   const [document, setDocument] = useState([]);
   const [documentFiles, setDocumentFiles] = useState([]);
+  const [approvalResponse, setApprovalResponse] = useState(null);
+  console.log(approvalResponse);
 
+  // states for document modal
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+
+  // states for approval modal
+  const [approvalModalIsOpen, setApprovalModalIsOpen] = useState(false);
+  const toggleApprovaModal = () => setApprovalModalIsOpen(!approvalModalIsOpen);
+  const [rejectionMessage, setRejectionMessage] = useState("");
+  const onSetRejectionMessage = (message) => {
+    setRejectionMessage(() => message);
+    console.log(message);
+  };
 
   const getRequirements = async () => {
     try {
@@ -54,10 +69,33 @@ const CommuterApprovalRequirements = ({ userId, getApprovals }) => {
     );
   };
 
+  // approve or reject the rider approval request
+  const onResponseRiderApproval = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5180/api/Approval/UserApprovalResponse?usertype=Commuter&userid=${userId}&response=${approvalResponse}&rejectionmessage=${rejectionMessage}`,
+        {
+          method: "PUT",
+        }
+      );
+      if (response.ok) {
+        toggleApprovaModal();
+        getRequirements();
+        getApprovals();
+        console.log({
+          commuterId: userId,
+          response: approvalResponse,
+          message: rejectionMessage,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     getRequirements();
   }, [userId]);
-
 
   return (
     <>
@@ -65,30 +103,38 @@ const CommuterApprovalRequirements = ({ userId, getApprovals }) => {
         isOpen={isOpen}
         untoggle={toggle}
         document={documentFiles}
-        userName={`${document.firstName} ${document.middleName && document.middleName[0]
-          }. ${document.lastName}`}
+        userName={`${document.firstName} ${
+          document.middleName && document.middleName[0]
+        }. ${document.lastName}`}
+      />
+      <RiderApprovalResponseConfirmationModal
+        isOpen={approvalModalIsOpen}
+        toggle={toggleApprovaModal}
+        response={approvalResponse}
+        onResponse={onResponseRiderApproval}
+        setRejectionMessage={onSetRejectionMessage}
+        rejectionMessage={rejectionMessage}
       />
       <div className="commuter-approval-rectangle-requiment">
         <Row>
           <div
             style={{
-              backgroundImage: `url(${document.profilePath != null
-                ? `http://localhost:5180/img/commuter_profile/${document.profilePath}`
-                : images1
-                })`,
+              backgroundImage: `url(${
+                document.profilePath != null
+                  ? `http://localhost:5180/img/commuter_profile/${document.profilePath}`
+                  : images1
+              })`,
               backgroundSize: "cover",
             }}
             className="rider-profile d-flex ms-auto me-auto mt-3"
           />
           <h5 style={{ textAlign: "center", marginTop: "10px" }}>
-            {document.firstName}{" "}
-            {document.middleName && document.middleName[0]}.{" "}
-            {document.lastName}
+            {document.firstName} {document.middleName && document.middleName[0]}
+            . {document.lastName}
           </h5>
           <div className="profile-line"></div>
         </Row>
         <Row>
-
           <CommuterApprovalViewRequirements
             viewText={"View OR file"}
             viewFileText={"View the uploaded files from commuter"}
@@ -127,14 +173,21 @@ const CommuterApprovalRequirements = ({ userId, getApprovals }) => {
             className="btn-rider-approval"
             color="success"
             style={{ borderRadius: 50, fontWeight: "bold" }}
+            onClick={() => {
+              setApprovalResponse(true);
+              toggleApprovaModal();
+            }}
           >
-
             <FontAwesomeIcon icon={faCircleCheck} /> &nbsp; Approve
           </Button>
           <Button
             className="btn-rider-approval"
             color="danger"
             style={{ borderRadius: 50, fontWeight: "bold" }}
+            onClick={() => {
+              setApprovalResponse(false);
+              toggleApprovaModal();
+            }}
           >
             <FontAwesomeIcon icon={faCircleXmark} /> &nbsp;Reject
           </Button>
